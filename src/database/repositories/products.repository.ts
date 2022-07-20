d740c1ac9a347e6e44ef15d6d885f469286a1c20
Gaëtan Gelineau <gaetan.gelineau@gmail.com>
2022-07-20 23:33:09 +0200

    feat: Add products database requests

--- src/database/repositories/products.repository.ts
+++ src/database/repositories/products.repository.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../config/database.service';
+import format from 'pg-format';
+import { Product } from 'src/products/types/product';
+import { Order } from 'src/products/types/order';
 
 @Injectable()
 export class ProductsRepository {
@@ -9,6 +12,23 @@ export class ProductsRepository {
     return await this.databaseService.query(
       'SELECT * FROM product WHERE stock > 0',
       [],
+    );
+  }
+
+  async decreaseStocks(orders: Order[]) {
+    await this.databaseService.query(
+      format(
+        'UPDATE product as p SET stock = p.stock - orders.quantity::int FROM (values %L) as orders (productid, quantity) WHERE p.id::text = orders.productid;',
+        orders.map((order) => [order.productId, order.quantity]),
+      ),
+      [],
+    );
+  }
+
+  async findByIds(productIds: string[]): Promise<Product[]> {
+    return await this.databaseService.query(
+      'SELECT id, price FROM product WHERE id = ANY($1::uuid[])',
+      [productIds],
     );
   }
 }
